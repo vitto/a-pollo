91dab0ff4082c1bec684e4b1616cc4207b65fb81
Vittorio Vittori <me@vittoriovittori.com>
2015-11-09 22:34:20 +0100

    Some change to logic

--- index.js
+++ index.js
@@ -6,19 +6,21 @@ var reader = require('./lib/reader'),
     Hexo = require('hexo'),
     fs = require('fs');
 
-var config = yaml.safeLoad(fs.readFileSync('./apollo.yml', 'utf-8'));
+var conf = yaml.safeLoad(fs.readFileSync('./apollo.yml', 'utf-8'));
 
 var runBuild = function() {
     var hexo, widgetFiles;
 
-    widgetFiles = reader.load(config.watchFolder);
+    widgetFiles = reader.load(conf.docs);
 
     hexo = new Hexo(process.cwd(), {
-        debug: true,
         config: './hexo/_config.yml'
     });
 
     shell.rm('-Rf', 'public');
+    shell.mkdir('hexo/source/_data');
+    shell.rm('-f', 'hexo/source/_data/apollo.yml');
+    shell.cp('-f', './apollo.yml', 'hexo/source/_data/apollo.yml');
 
     hexo.init().then(function(){
 
@@ -26,7 +28,7 @@ var runBuild = function() {
 
         for (var i = 0; i < widgetFiles.length; i += 1) {
             postData = formatter.toHexo(widgetFiles[i]);
-            postData.content = widget.toMarkdown(widgetFiles[i], config);
+            postData.content = widget.toMarkdown(widgetFiles[i], conf.author);
             hexo.post.create(postData, true);
         }
 
