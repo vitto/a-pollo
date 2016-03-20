f8746bbbb80cf906d74fcf503751e2537e918d7b
fain182 <fain182@gmail.com>
2016-03-20 15:40:55 +0100

    Added other tests for parser

--- spec/parserSpec.js
+++ spec/parserSpec.js
@@ -48,5 +48,45 @@ describe("CSS comment parser", function() {
         var widget = result[0];
         expect(widget.text).toEqual("This should be used to connect social accounts to the project service.\n It requires Font Awesome http://fontawesome.github.io to be loaded as dependency.\n The code above will show the Facebook version.");
     });
+    it("parses html and remove new lines", function() {
+        var result = parser.format(completeDocumentationExample, "style.css");
+        var widget = result[0];
+        expect(widget.html).toEqual(
+            '<a href="#" class="button-social button-social--facebook">' +
+            '<div class="button-social__icon">' +
+            '<i class="fa fa-facebook"></i>' +
+            '</div>' +
+            '<div class="button-social__network">' +
+            'facebook' +
+            '</div>' +
+            '</a>'
+        );
+    });
+    it("gives html beautified", function() {
+        var result = parser.format(completeDocumentationExample, "style.css");
+        var widget = result[0];
+        expect(widget.htmlSnippet).toEqual(
+            '<a href="#" class="button-social button-social--facebook">' + "\n" +
+            '    <div class="button-social__icon"><i class="fa fa-facebook"></i></div>' + "\n" +
+            '    <div class="button-social__network">facebook</div>' + "\n" +
+            '</a>'
+        );
+    });
+    it("parses authors", function() {
+        var result = parser.format(completeDocumentationExample, "style.css");
+        var widget = result[0];
+        expect(widget.auth).toEqual('[Vittorio Vittori](http://vit.to)');
+    });
+    it("parses filename removing directory", function() {
+        var result = parser.format(completeDocumentationExample, "example/text/style.css");
+        var widget = result[0];
+        expect(widget.file).toEqual('style.css');
+    });
+    it("parses file path and name", function() {
+        var result = parser.format(completeDocumentationExample, "example/text/style.css");
+        var widget = result[0];
+        expect(widget.path).toEqual('example/text/style.css');
+        expect(widget.file).toEqual('style.css');
+    });
 
 });
