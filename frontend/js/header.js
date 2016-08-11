8b8a8b1ecc819e1f3e33b9618ee1fbf3f89bbcb8
Vittorio Vittori <me@vittoriovittori.com>
2016-08-11 23:44:58 +0200

    Changes how header interacts with page scrolling gestures

--- frontend/js/header.js
+++ frontend/js/header.js
@@ -1,20 +1,26 @@
 $(function(){
-  var $header, startFrom, headerHeight, scrollTop, prevScroll;
+  var $header, startFrom, headerHeight, scrollTop, prevScroll, diffScroll, friction;
   $header = $('.miniml-header--fixed');
   headerHeight = 60;
+  friction = 3;
   startFrom = headerHeight;
   scrollTop = 0;
   prevScroll = 0;
+  diffScroll = 0;
 
   $(window).scroll(function () {
     scrollTop = $(window).scrollTop();
     if (prevScroll > scrollTop) {
-      if (scrollTop > startFrom) {
+      diffScroll = diffScroll - 1 > 0 ? diffScroll - 1 : 0 ;
+      if (diffScroll === 0) {
         $header.removeClass('miniml-header--hidden');
         $header.removeClass('miniml-header--outside');
       }
     } else {
-      $header.addClass('miniml-header--outside');
+      diffScroll = diffScroll + 1 < headerHeight / friction ? diffScroll + 1 : headerHeight / friction ;
+      if (diffScroll >= headerHeight / friction) {
+        $header.addClass('miniml-header--outside');
+      }
     }
     if (scrollTop === 0) {
       $header.addClass('miniml-header--hidden');
