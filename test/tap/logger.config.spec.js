98be5c2145b2bd59be5bedcc35f2146ec5dfc918
Vittorio Vittori <me@vittoriovittori.com>
2016-12-15 18:58:57 +0100

    Changes some test

--- test/tap/logger.config.spec.js
+++ test/tap/logger.config.spec.js
@@ -2,6 +2,11 @@ const logger = require('../../lib/logger')
 const tap = require('tap')
 const test = tap.test
 
+test('config log level defaults', tap => {
+  tap.plan(1)
+  tap.equal(logger.logLevel(), 3)
+})
+
 test('config log level with valid setting', tap => {
   tap.plan(8)
   tap.equal(logger.logLevel(0), 0)
@@ -16,7 +21,7 @@ test('config log level with valid setting', tap => {
 
 test('config log level with not valid setting', tap => {
   var defaultVerbosity = 3
-  tap.plan(8)
+  tap.plan(7)
   tap.equal(logger.logLevel(4), defaultVerbosity)
   tap.equal(logger.logLevel('4'), defaultVerbosity)
   tap.equal(logger.logLevel(-1), defaultVerbosity)
@@ -24,5 +29,4 @@ test('config log level with not valid setting', tap => {
   tap.equal(logger.logLevel('33'), defaultVerbosity)
   tap.equal(logger.logLevel('-1'), defaultVerbosity)
   tap.equal(logger.logLevel('#'), defaultVerbosity)
-  tap.equal(logger.logLevel(), defaultVerbosity)
 })
